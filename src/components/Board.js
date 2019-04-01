2d0b57039dfaf945388f1cca88a172f5c669879e
GreedoRen <greedo.it@gmail.com>
2019-04-02 00:05:56 +0300

    Replace some feature

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,18 +2,11 @@ import React from 'react';
 import Row from './Row';
 
 const Board = ({
-  onClick,
-  rows,
-}) => (
+  rows}) => (
   <div>
     {rows.map((row, index) => (
       <Row
-        key={index}
         boxes={row}
-        firstRow={index === 0}
-        lastRow={index === rows.length - 1}
-        onClick={onClick}
-        rowIndex={index}
       />
     ))}
   </div>
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,34 +1,12 @@
 import React from 'react';
-import { css } from 'react-emotion';
-
 import Box from './Box';
 
-const rowStyle = css({
-  display: 'flex',
-  justifyContent: 'center',
-});
 
 const Row = ({
-  boxes,
-  firstRow,
-  lastRow,
-  onClick,
-  rowIndex,
-}) => (
-  <div className={rowStyle}>
+  boxes}) => (
+  <div className='Row'>
     {boxes.map((box, index) => (
-      <Box
-        columnIndex={index}
-        key={index}
-        firstColumn={index === 0}
-        firstRow={firstRow}
-        lastColumn={index === boxes.length - 1}
-        lastRow={lastRow}
-        onClick={onClick}
-        rowIndex={rowIndex}
-        size={100}
-        value={box}
-      />
+      <Box  />
     ))}
   </div>
 );
